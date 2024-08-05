351a6f62dffac7ff499f7afadf96e8da977e31f7
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2024-08-05 12:59:54 -0300

    Initial project setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# MADR
+
+**Meu Acervo Digital de Romances** é o projeto final do
+[curso de FastAPI](https://fastapidozero.dunossauro.com/) do
+[@dunossauro](https://github.com/dunossauro). A seguinte página possui as especificações completas do desafio:
+[Projeto final](https://fastapidozero.dunossauro.com/14/).
---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from madr.app import app
+
+
+def test_root_should_return_ok_and_hello_world():
+    client = TestClient(app)
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Hello, World!'}
