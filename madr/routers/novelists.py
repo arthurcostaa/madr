9fdaea31b753445dcdae7b42959f3ceb687eb539
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2024-08-29 00:19:23 -0300

    Add delete endpoint to novelists

--- madr/routers/novelists.py
+++ madr/routers/novelists.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 
 from madr.database import get_session
 from madr.models import Novelist, User
-from madr.schemas import NovelistPublic, NovelistSchema
+from madr.schemas import Message, NovelistPublic, NovelistSchema
 from madr.security import get_current_user
 from madr.utils import sanitize
 
@@ -40,3 +40,23 @@ def create_novelist(
     session.refresh(db_novelist)
 
     return db_novelist
+
+
+@router.delete(
+    '/{novelist_id}', status_code=HTTPStatus.OK, response_model=Message
+)
+def delete_novelist(novelist_id: int, session: T_Session, user: T_CurrentUser):
+    novelist_db = session.scalar(
+        select(Novelist).where(Novelist.id == novelist_id)
+    )
+
+    if not novelist_db:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='Novelist not found in MADR',
+        )
+
+    session.delete(novelist_db)
+    session.commit()
+
+    return {'message': 'Novelist deleted from MADR'}
--- tests/test_novelists.py
+++ tests/test_novelists.py
@@ -21,3 +21,23 @@ def test_create_existent_novelist(client, token, novelist):
 
     assert response.status_code == HTTPStatus.BAD_REQUEST
     assert response.json() == {'detail': 'Novelist already exists.'}
+
+
+def test_delete_novelist(client, novelist, token):
+    response = client.delete(
+        f'/novelists/{novelist.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Novelist deleted from MADR'}
+
+
+def test_delete_unexistent_novelist(client, novelist, token):
+    response = client.delete(
+        f'/novelists/{novelist.id + 1}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'Novelist not found in MADR'}
